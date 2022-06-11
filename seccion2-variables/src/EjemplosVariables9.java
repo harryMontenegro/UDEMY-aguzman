da86883a8d8e8ef5e643d4206212721ce8919905
harryMontenegro <harryMontenegro@gmail.com>
2022-06-11 18:16:24 -0500

    Variables3

---
+++ seccion2-variables/src/EjemplosVariables9.java
@@ -0,0 +1,25 @@
+
+public class EjemplosVariables9 {
+    public static void main(String[] args) {
+
+        String saludar = "Hola Mundo desde Java";
+        System.out.println(saludar);
+        System.out.println("saludar.toUpperCase() = " + saludar.toUpperCase());
+
+        int numero = 10;
+        System.out.println("numero = " + numero);
+
+        boolean valor = true;
+        int numero2 = 5;
+
+        if(valor){
+            System.out.println("numero = " + numero);
+            numero2 = 10;
+        }
+        System.out.println("numero2 = " + numero2);
+
+        //El tipo de datos var permite asignar un valor de tipo flexible, donde influye el
+        // valor para definir el tipo de variable
+        var numero3 = "15";
+    }
+}
---
+++ seccion2-variables/src/ReglasParaDefinirVariables10.java
@@ -0,0 +1,36 @@
+
+public class ReglasParaDefinirVariables10 {
+    public static void main(String[] args) {
+
+        String saludar = "Hola Mundo desde Java";
+        System.out.println(saludar);
+        System.out.println("saludar.toUpperCase() = " + saludar.toUpperCase());
+
+        int numero = 10;
+        System.out.println("numero = " + numero);
+
+        boolean valor = true;
+        int numero2 = 5;
+
+        if(valor){
+            System.out.println("numero = " + numero);
+            numero2 = 11;
+        }
+        System.out.println("numero2 = " + numero2);
+
+        //El tipo de datos var permite asignar un valor de tipo flexible, donde influye el
+        // valor para definir el tipo de variable
+        var numero3 = "15";
+
+        String nombre;
+
+        nombre = "Andrés";
+
+        if(numero > 10){
+            nombre = "Juan";
+        }
+        System.out.println("nombre = " + nombre);
+
+        int edadPersona = 5;
+    }
+}
