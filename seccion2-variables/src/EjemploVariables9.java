1b76da649d9226bac2dec0c1dce57e2f1eb53b86
harrymontenegro <harryjm018@hotmail.com>
2022-06-11 09:08:57 -0500

    Primera clase Andrez Guzman

---
+++ seccion2-variables/src/EjemploVariables9.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class EjemploVariables9 {
+    public static void main(String[] args) {
+
+        String saludar = "Hola Mundo desde Java";
+        System.out.println(saludar);
+
+        System.out.println("saludar.toUpperCase() = " + saludar.toUpperCase());
+
+        int numero = 10;
+
+    }
+}
