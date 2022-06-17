8f47dd6ee0000caa37ef05d680d2ddb208c2a2fc
harryMontenegro <harryMontenegro@gmail.com>
2022-06-16 22:02:07 -0500

    EjemploStringValidar30

---
+++ seccion3/src/tipostring/cadenas/EjemploStringValidar30.java
@@ -0,0 +1,38 @@
+package tipostring.cadenas;
+
+public class EjemploStringValidar30 {
+    public static void main(String[] args) {
+
+        String curso = null;
+
+        boolean esNulo = curso == null;
+
+        System.out.println("esNulo = " + esNulo);
+
+        if (esNulo) {
+            curso = "Programacion Java";
+        }
+
+        boolean esVacio = curso.length() == 0;
+
+        System.out.println("esVacio = " + esVacio);
+
+        boolean esVacio2 = curso.isEmpty();
+        System.out.println("esVacio2 = " + esVacio2);
+
+
+/*        if (!esVacio2) {
+            System.out.println(curso.toUpperCase());
+            System.out.println("Bienvenido al curso ".concat(curso));
+        }*/
+
+        // isBlank() es más estricto que isEmpty(), isBlank() valida que sea distinto de null, luego valida que sea
+        // distinto de isEmpty y además que el contenido sea distinto a un espacio en blanco.
+        boolean esBlanco = curso.isBlank();
+        System.out.println("esBlanco = " + esBlanco);
+        if(!esBlanco){
+            System.out.println(curso.toUpperCase());
+            System.out.println("Bienvenido al curso ".concat(curso));
+        }
+    }
+}
