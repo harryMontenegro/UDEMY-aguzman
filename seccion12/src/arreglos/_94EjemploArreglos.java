c7c642b0c9956971947f99e03521f518bf4307f5
harryMontenegro <harryMontenegro@gmail.com>
2022-06-26 13:52:04 -0500

    arreglos1

---
+++ seccion12/src/arreglos/_94EjemploArreglos.java
@@ -0,0 +1,25 @@
+package arreglos;
+
+public class _94EjemploArreglos {
+    public static void main(String[] args) {
+
+        int[] numeros = new int[4];
+
+        numeros[0] = 10;
+        numeros[1] = Integer.valueOf("7");
+        numeros[2] = 35;
+        numeros[3] = -1;
+        //numeros[4] = 5;
+
+        int i = numeros[0];
+        int j = numeros[1];
+        int k = numeros[2];
+        int l = numeros[numeros.length - 1];
+        //int m = numeros[4];
+
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+        System.out.println("k = " + k);
+        System.out.println("l = " + l);
+    }
+}
