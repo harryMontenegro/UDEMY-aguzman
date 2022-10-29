af35968014398cb7d5cc8f1fd9e6bbb4fc1f639b
harrymontenegro <harryjm018@hotmail.com>
2022-10-29 16:18:37 -0500

    varias pruebas 2

--- proof/src/pruebas_anymatch_nonematch/PruebaAnyMatchNoneMatch.java
+++ proof/src/pruebas_anymatch_nonematch/PruebaAnyMatchNoneMatch.java
@@ -11,7 +11,7 @@ public class PruebaAnyMatchNoneMatch {
         List<String> creditosInt = Arrays.asList("CR0122","CR0124","CR0123", "CR0125");
         List<String> creditos = new ArrayList<>();
 
-        List<PolizaCreditoRecogidos> lista = Arrays.asList(
+        List<PolizaCreditoRecogidos> registrosGuardados = Arrays.asList(
                 new PolizaCreditoRecogidos("CR0122", true),
                 new PolizaCreditoRecogidos("CR0123", true),
                 new PolizaCreditoRecogidos("CR0124", false));
@@ -19,11 +19,15 @@ public class PruebaAnyMatchNoneMatch {
         //List<pruebas_anymatch_nonematch.PolizaCreditoRecogidos> lista = new ArrayList<>();
 
 
-        creditos = creditosInt.stream().filter(cre -> lista.stream().noneMatch(ls -> ls.getNumeroCredito().equals(cre))).collect(Collectors.toList());
+        creditos = creditosInt.stream()
+                .filter(numeroCredito -> registrosGuardados
+                        .stream()
+                        .noneMatch(ls -> ls.getNumeroCredito().equals(numeroCredito)))
+                .collect(Collectors.toList());
 
         List<PolizaCreditoRecogidos> polCredRecLista = new ArrayList<>();
 
-        if(creditos.stream().anyMatch(lc -> lista.stream().noneMatch(pc -> pc.getNumeroCredito().equals(lc)))){
+        if(creditos.stream().anyMatch(c -> registrosGuardados.stream().noneMatch(pc -> pc.getNumeroCredito().equals(c)))){
             List<PolizaCreditoRecogidos> listafinal = new ArrayList<>();
             creditos.forEach(ele -> {
                 PolizaCreditoRecogidos pol = new PolizaCreditoRecogidos(ele, true);
@@ -34,14 +38,14 @@ public class PruebaAnyMatchNoneMatch {
         }
 
 
-        polCredRecLista = lista
+        polCredRecLista = registrosGuardados
                 .stream()
                 .filter(pcrguardado -> creditosInt.stream().anyMatch(lcr -> lcr.equals(pcrguardado.getNumeroCredito())))
                 .peek(pcrguardado -> pcrguardado.setIndicadorHabilitado(true))
                 .collect(Collectors.toList());
 
         polCredRecLista.addAll(
-                lista
+                registrosGuardados
                     .stream()
                     .filter(pcrguardado -> creditosInt.stream().noneMatch(lcr -> lcr.equals(pcrguardado.getNumeroCredito())))
                     .peek(pcrguardado -> pcrguardado.setIndicadorHabilitado(false)).toList()
